7dd0681fccec472783252adb9db0fcec0dd9f3c9
cliffamadeus <dev.clfm5t12@gmail.com>
2025-03-04 16:25:31 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'aha-ka',
+  webDir: 'dist'
+};
+
+export default config;
